5cdfb569cce68b90b9babbfbfc864f61d1d5c470
lee-shun <lee@wb.com>
2020-11-05 10:52:48 +0800

    install_zsh v1.0

--- Auto_scripts/install_zsh.sh
+++ Auto_scripts/install_zsh.sh
@@ -9,3 +9,11 @@ git clone https://github.com/lee-shun/.oh-my-zsh.git
 git clone https://github.com/lee-shun/lee_config_files.git
 
 cp -r ~/lee_config_files/.zshrc ~
+
+echo "change the default bash to zsh? yes or not."
+read change_flag
+if change_flag=="yes"
+then
+    chsh -s /bin/zsh
+    echo "change finish, please reboot..."
+fi
