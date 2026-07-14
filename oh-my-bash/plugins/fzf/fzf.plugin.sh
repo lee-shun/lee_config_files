6c6b2a855c9a43cc368b4c0a6a99068e88bf6156
Shun Li <is.shun.li@outlook.com>
2026-07-14 18:33:28 +0800

    update

--- oh-my-bash/plugins/fzf/fzf.plugin.sh
+++ oh-my-bash/plugins/fzf/fzf.plugin.sh
@@ -1,11 +1,11 @@
 #! bash oh-my-bash.module
 
 # Check if fzf is installed
+if _omb_util_command_exists fzf; then
 if [ -f ~/.fzf.bash ]; then
     source ~/.fzf.bash
+fi
     eval "$(fzf --bash)"
 else
     echo '[oh-my-bash] fzf not found, please install it from https://github.com/junegunn/fzf'
 fi
-
-
